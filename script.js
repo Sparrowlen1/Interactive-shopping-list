a963208096535af92662af2b79afa48960127b0a
Daniel Muthui <dannymuthui118@gmail.com>
2026-04-06 14:33:54 +0300

    feat: defined the DOM elements

---
+++ script.js
@@ -0,0 +1,11 @@
+let shoppingItems = [];
+let nextId = 1;
+
+const itemInput = document.getElementById('itemInput');
+const itemCostInput = document.getElementById('itemCostInput');
+const addBtn = document.getElementById('addBtn');
+const clearAllBtn = document.getElementById('clearAllBtn');
+const shoppingListUl=document.getElementById('shoppingListUl');
+const emptyPlaceholder=document.getElementById('emptyPlaceholder');
+const itemCountSpan=document.getElementById('itemCount');
+const totalCostDisplaySpan=document.getElementById('totalCostDisplay');
